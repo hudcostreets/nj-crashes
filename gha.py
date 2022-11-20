f47c80481d182ae9db5b7d5a3a25f5c39563a3ef
Ryan Williams <ryan@runsascoded.com>
2022-11-19 19:52:08 -0500

    don't store output nb

--- gha.py
+++ gha.py
@@ -6,7 +6,6 @@ from utz import check, process, run
 nb = 'parse-njsp-xmls.ipynb'
 out = f'out/{nb}'
 
-
 def configure_author(name, email):
     run('git', 'config', '--global', 'user.name', name)
     run('git', 'config', '--global', 'user.email', email)
@@ -14,7 +13,7 @@ def configure_author(name, email):
 
 @click.command()
 @click.option('-c', '--configure-author', 'do_configure_author', is_flag=True)
-@click.option('-f', '--force', count=True, help=f'1x: continue past initial no-op data update; 2x: commit no-op papermill run (with spurious/timestamp changes to {out})')
+@click.option('-f', '--force', count=True, help=f'Continue past initial no-op data update')
 @click.option('-p', '--push', is_flag=True)
 def main(do_configure_author, force, push):
     run('./refresh-data.sh')
@@ -26,31 +25,37 @@ def main(do_configure_author, force, push):
         else:
             return
 
+    did_commit = False
     def commit(msg):
-        nonlocal do_configure_author
+        nonlocal do_configure_author, did_commit
         if do_configure_author:
             configure_author('GitHub Actions', 'ryan-williams@users.noreply.github.com')
             do_configure_author = False
         run('git', 'commit', '-am', msg)
+        did_commit = True
 
     if not git_is_clean:
         commit('GHA: update data')
 
     run('papermill', nb, out)
-    changed_files = [ line[3:] for line in process.lines('git', 'status', '--porcelain') ]
-    print(f'{len(changed_files)} changed files:')
-    for f in changed_files:
-        print(f'\t{f}')
-    if len(changed_files) == 1 and changed_files[0] == out:
+    git_is_clean = check('git', 'diff', '--quiet', 'HEAD')
+    if git_is_clean:
         print('No plot/data changes found')
-        if force >= 2:
-            print(f'force={force}; continuing')
-        else:
-            return
+    else:
+        changed_files = [ line[3:] for line in process.lines('git', 'status', '--porcelain') ]
+        print(f'{len(changed_files)} changed files:')
+        for f in changed_files:
+            print(f'\t{f}')
 
-    commit('GHA: update data/plots')
+        if changed_files == ['njsp.db']:
+            print('Skipping spurious DB change')
+        else:
+            commit('GHA: update data/plots')
     if push:
-        run('git', 'push', 'origin')
+        if did_commit:
+            run('git', 'push', 'origin')
+        else:
+            print('Nothing to push')
 
 
 if __name__ == '__main__':
