ca8c945ff3ec62bd5b6a55497590c1353acc62b6
Ryan Williams <ryan@runsascoded.com>
2022-12-02 21:00:25 -0500

    plot tweaks, factor notebooks

---
+++ utils.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+
+def get_children(tag):
+    return [ child for child in tag.children if not isinstance(child, str) ]
+
+
+def parse_file(path):
+    with open(path, 'r') as f:
+        xml = bs(f, features="xml")
+    children = list(xml.children)
+    assert len(children) == 2
+    fauqstats = children[-1]
+    assert fauqstats.name == 'FAUQSTATS', fauqstats.name
+    rundate = fauqstats.RUNDATE.text
+    year = int(fauqstats.STATSYEAR.text)
+    counties = fauqstats.find_all('COUNTY', recursive=False)
+    total_accidents = int(fauqstats.TOTACCIDENTS.text)
+    total_injuries = int(fauqstats.TOTINJURIES.text)
+    total_fatalities = int(fauqstats.TOTFATALITIES.text)
+    crash_counties = [ county for county in counties if county.MUNICIPALITY ]
+    print(f'{len(counties)} "COUNTY" entries, {len(crash_counties)} containing "MUNICIPALITY"/crash info, {total_accidents} accidents, {total_injuries} injuries, {total_fatalities} fatalities')
+    records = []
+    for county in crash_counties:
+        municipality = county.MUNICIPALITY
+        assert municipality.name == 'MUNICIPALITY'
+        children = get_children(municipality)
+        accidents = municipality.find_all('ACCIDENT', recursive=False)
+        if len(children) != len(accidents):
+            raise ValueError(f'Found {len(children)} municipality children, but {len(accidents)} accidents: {county}. {accidents}')
+        for accident in accidents:
+            obj = { child.name: child.text for child in get_children(accident) }
+            obj = dict(**county.attrs, **municipality.attrs, **accident.attrs, **obj, )
+            records.append(obj)
+
+    df = pd.DataFrame(records)
+    totals_df = pd.DataFrame([dict(
+        year=year,
+        accidents=total_accidents,
+        injuries=total_injuries,
+        fatalities=total_fatalities,
+    )])
+    return dict(crashes=df, totals=totals_df, rundate=rundate)
