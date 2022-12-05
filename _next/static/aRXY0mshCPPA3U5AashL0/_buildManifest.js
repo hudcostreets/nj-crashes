011209634bdb15132635b0107c3ac5ec2fff27c4
ryan-williams <nobigdealstyle@gmail.com>
2022-12-05 16:21:56 +0000

    Deploying to gh-pages from @ neighbor-ryan/nj-fatal-crashes@03c750db7125e470672ee6d9d229a12ac74aafa1 🚀

---
+++ _next/static/aRXY0mshCPPA3U5AashL0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0ad90872b17c6f69.css","static/chunks/pages/index-203c1b0230dd7296.js"],"/_error":["static/chunks/pages/_error-c1fe5748fb991da7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
