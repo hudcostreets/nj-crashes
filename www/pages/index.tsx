e71664f46d62f7b87f87483772e5b73856b4c819
Ryan Williams <ryan@runsascoded.com>
2022-12-14 09:03:20 -0500

    re-order plots, tweak padding/copy

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -37,9 +37,9 @@ type Plot = PlotSpec & {
 
 const EMPTY: PlotSpec[] = []
 const SC_MY_IPD_SPECS: PlotSpec[] =
-    EMPTY.concat(...['m', 'y'].map(my =>
+    EMPTY.concat(...['y', 'm'].map(my =>
         EMPTY.concat(...['s', 'c'].map(sc =>
-            EMPTY.concat(...['i', 'p', 'd'].map(t => {
+            EMPTY.concat(...['p', 'i', 'd'].map(t => {
                 const id = `${t}${sc}${my}`
                 const name = `njdot/${id}`
                 const region = { 's': 'State', 'c': 'County' }[sc]
@@ -202,10 +202,10 @@ const Home = ({ plotsDict, projectedTotals, rundate, }: Props) => {
     const sections = plots.map(({ id, title, menuName, dropdownSection, }) => ({ id, name: menuName || title, dropdownSection: dropdownSection }))
     const menus = [
         { id: "NJSP", name: "NJSP", },
-        { id: "state-months", name: "State x Months", },
-        { id: "county-months", name: "Counties x Months", },
         { id: "state-years", name: "State x Years", },
         { id: "county-years", name: "Counties x Years", },
+        { id: "state-months", name: "State x Months", },
+        { id: "county-months", name: "Counties x Months", },
     ].map(s => ({
         ...s,
         sections: sections.filter(({ name, dropdownSection }) => s.name == dropdownSection)
@@ -236,7 +236,7 @@ const Home = ({ plotsDict, projectedTotals, rundate, }: Props) => {
                     The NJ State Police <A title={"NJ State Police fatal crash data"} href={"https://nj.gov/njsp/info/fatalacc/"}>publish fatal crash data</A> going back to 2008. {"It's usually current to the previous day, though things also show up weeks or months after the fact. The first 5 plots below are from that data."}
                 </p>
                 <p>
-                    <a href={"#njdot"}>Below that</a> is some analysis of <A title={"NJ DOT raw crash data"} href={"https://www.state.nj.us/transportation/refdata/accident/rawdata01-current.shtm"}>NJ DOT raw crash data</A>, which includes injury and property-damage crashes from 2001-2020 (≈6MM records). {`It's a richer dataset, but less up to date (it currently ends in 2020, just before things regressed dramatically in 2021). 2021 data should arrive in early 2023.`}
+                    <a href={"#njdot"}>Below that</a> is some analysis of <A title={"NJ DOT raw crash data"} href={"https://www.state.nj.us/transportation/refdata/accident/rawdata01-current.shtm"}>NJ DOT raw crash data</A>, which includes property-damage, injury, and fatal crashes from 2001-2020 (≈6MM records). {`It's a richer dataset, but less up to date (it currently ends in 2020, just before things regressed dramatically in 2021). 2021 data should arrive in early 2023.`}
                 </p>
                 <p>{`Tap plots to see specific values, single- or double-tap legend entries to toggle or "solo" them.`}</p>
                 <p>Code and cleaned data are on GitHub <A href={GitHub.href}>here</A>.</p>
@@ -247,9 +247,9 @@ const Home = ({ plotsDict, projectedTotals, rundate, }: Props) => {
                                 idx == menus[0].sections.length && <>
                                     <h1 id={"njdot"}><a href={`#njdot`}>NJ DOT Raw Crash Data</a></h1>
                                     <p>
-                                        NJ DOT <A title={"NJ DOT raw crash data"} href={"https://www.state.nj.us/transportation/refdata/accident/rawdata01-current.shtm"}>publishes raw crash data</A>, including injury and property-damage crashes, going back to 2001 (≈6MM records).
+                                        NJ DOT <A title={"NJ DOT raw crash data"} href={"https://www.state.nj.us/transportation/refdata/accident/rawdata01-current.shtm"}>publishes raw crash data</A>, including property-damage, injury, and fatal crashes, going back to 2001 (≈6MM records).
                                     </p>
-                                    <p>{"It generally shows a downward trend over the last few decades, which is good, though it ends at 2020, just before we regressed by 10-15yrs (based on the NJSP data above). 2021 data should land in early 2023."}</p>
+                                    <p>{"The data currently ends in 2020, after a drop in all types of crashes due to COVID, and just before a spike in all crash types in 2021 and 2022 (based on the NJSP data above, and other sources). 2021 data should land in early 2023."}</p>
                                 </>
                             }
                             <div key={id} className={styles["plot-container"]}>
