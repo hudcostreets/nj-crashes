968dcd6437d8918ea31157e60c887e2494d426b2
Ryan Williams <ryan@runsascoded.com>
2023-01-01 11:37:44 -0500

    override social icon dark-mode

---
+++ www/src/socials.css.ts
@@ -0,0 +1,12 @@
+import {globalStyle} from '@vanilla-extract/css'
+import * as css from "next-utils/socials.css";
+
+export const svg = css.svg
+globalStyle(`.${css.socials} path, .${css.socials} polygon`, {
+    fill: "black",
+    "@media": {
+        "(prefers-color-scheme: dark)": {
+            fill: "black",  // no dark mode implemented yet, need this to still be black since page background is white
+        }
+    }
+})
--- www/src/socials.tsx
+++ www/src/socials.tsx
@@ -1,8 +1,9 @@
 import * as Socials from "next-utils/socials"
+import * as css from "./socials.css"
 
 export const url = "https://neighbor-ryan.org/nj-crashes"
 
-export const GitHub: Socials.Social = Socials.GitHub("neighbor-ryan/nj-crashes")
+export const GitHub: Socials.Social = Socials.GitHub("neighbor-ryan/nj-crashes", css.svg)
 export const Twitter = Socials.Twitter("RunsAsCoded")
 export const Instagram = Socials.Instagram("neighbor_ryan")
 export const YouTube = Socials.YouTube("@neighborryan")
