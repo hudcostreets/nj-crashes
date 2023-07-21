798196dd88852eb701ab1d33325a571859a0cbce
Ryan Williams <ryan@runsascoded.com>
2023-07-20 23:32:57 -0400

    commit_crashes cli

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+packages = find_packages()
+print(f'packages: {packages}')
+
 setup(
     name="nj_crashes",
     install_requires=open("requirements.txt", "r").read(),
-    packages=find_packages(),
+    packages=packages,
+    entry_points='''
+        [console_scripts]
+        commit_crashes=nj_crashes.commit_crashes:main
+    ''',
 )
