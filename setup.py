ee00e3f8742eed3a652210e2d92392489eeea5a3
Ryan Williams <ryan@runsascoded.com>
2023-07-21 09:39:37 -0400

    factor out `pub` branch

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup, find_packages
 
-packages = find_packages()
-print(f'packages: {packages}')
-
 setup(
     name="nj_crashes",
     install_requires=open("requirements.txt", "r").read(),
-    packages=packages,
-    entry_points='''
-        [console_scripts]
-        commit_crashes=nj_crashes.commit_crashes:main
-    ''',
+    packages=find_packages(),
 )
