fb732efb3eb39d22afe9736059dfff86010c367b
Ryan Williams <ryan@runsascoded.com>
2023-07-24 08:15:08 -0400

    rm broken import

--- njsp/cli/main.py
+++ njsp/cli/main.py
@@ -2,7 +2,7 @@ from njsp.cli.base import njsp
 from .refresh_data import refresh_data
 from .update_pqts import update_pqts
 from .update_plots import update_plots
-from .slack import slack, commit, sync
+from .slack import slack, sync
 
 
 def main():
