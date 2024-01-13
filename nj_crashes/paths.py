5dc0e45f254e4e900ca263d70f895e22885f62c6
Ryan Williams <ryan@runsascoded.com>
2024-01-12 22:42:58 -0500

    more paths

--- nj_crashes/paths.py
+++ nj_crashes/paths.py
@@ -1,11 +1,18 @@
-from os.path import basename, dirname
+from os.path import basename, dirname, join
 
 PKG_DIR = dirname(__file__)
 ROOT_DIR = dirname(PKG_DIR)
-WWW_DIR = f'{ROOT_DIR}/www'
-PUBLIC_DIR = f'{WWW_DIR}/public'
-PLOTS_DIR = f'{PUBLIC_DIR}/plots'
+
+DATA_DIR = join(ROOT_DIR, 'data')
+NJDOT_DIR = join(ROOT_DIR, 'njdot')
+DOT_DATA = join(NJDOT_DIR, 'data')
+SRI_DIR = join(ROOT_DIR, '.sri')
+WWW_DIR = join(ROOT_DIR, 'www')
+PUBLIC_DIR = join(WWW_DIR, 'public')
+WWW_DOT = join(PUBLIC_DIR, 'njdot')
+PLOTS_DIR = join(PUBLIC_DIR, 'plots')
 PKG_NAME = basename(PKG_DIR)
-RUNDATE_PATH = f'{PUBLIC_DIR}/rundate.json'
-DB_PATH = f'{ROOT_DIR}/{PKG_NAME}.db'
+RUNDATE_PATH = join(PUBLIC_DIR, 'rundate.json')
+DB_PATH = join(ROOT_DIR, f'{PKG_NAME}.db')
+
 DB_URI = f'sqlite:///{DB_PATH}'
