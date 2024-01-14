e085afd4ba5b3eab618b84574ec9c5ac09e76914
Ryan Williams <ryan@runsascoded.com>
2024-01-14 18:43:08 -0500

    njsp-plots: update data/njsp/year-type-county.csv

--- njsp/paths.py
+++ njsp/paths.py
@@ -6,10 +6,12 @@ from nj_crashes.paths import DATA_DIR
 CRASHES_PQT = join(DATA_DIR, 'crashes.pqt')
 NJSP_DATA = join(DATA_DIR, 'njsp')
 # Tabula template JSONs and NJSP summary PDFs live in this dir
-NJSP_STATS = join(NJSP_DATA, 'stats')
 ANNUAL_REPORTS = join(NJSP_DATA, 'annual-reports')
+ANNUAL_SUMMARIES = join(NJSP_DATA, 'annual-summaries')
 ANNUAL_YTC = join(ANNUAL_REPORTS, 'year-type-county')
 MISSING_YTC = join(ANNUAL_YTC, 'missing_ytc.csv')
 
-YT_CSV = path.join(NJSP_STATS, 'year-type.csv')
-YTC_CSV = path.join(NJSP_STATS, 'year-type-county.csv')
+ANNUAL_SUMMARIES_YT_CSV = path.join(ANNUAL_SUMMARIES, 'year-type.csv')
+ANNUAL_SUMMARIES_YTC_CSV = path.join(ANNUAL_SUMMARIES, 'year-type-county.csv')
+
+YTC_CSV = path.join(NJSP_DATA, 'year-type-county.csv')
