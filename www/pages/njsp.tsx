c66b08001c325c8accc0d659c7b77764fd7e8807
Ryan Williams <ryan@runsascoded.com>
2024-01-15 11:06:22 -0500

    wip njsp.tsx

---
+++ www/pages/njsp.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react'
+import type { GetStaticProps } from "next";
+import { loadSync } from "next-utils/load";
+import { loadPlot } from "next-utils/plot-load";
+import { HasTotals, njspPlotSpec, ProjectedTotals } from "@/src/plotSpecs";
+import css from "@/pages/index.module.scss";
+import { Head } from "next-utils/head";
+import { GitHub, url } from "@/src/socials";
+import A from "next-utils/a";
+import { Socials } from "next-utils/socials";
+import { PlotParams } from "react-plotly.js";
+import PlotWrapper from "next-utils/plot-wrapper";
+import { initDuckDb, runQuery } from "next-utils/parquet";
+import { AsyncDuckDB } from "@duckdb/duckdb-wasm";
+import path, { dirname } from "path"
+import fs from "fs";
+
+// type Props = { params: PlotParams, rundate: string, } & HasTotals
+export type Props = {
+    params: PlotParams
+    // crashesPqtArr: number[]
+    crashesBase64: string
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    // const { rundate } = loadSync<{ rundate: string }>(`public/rundate.json`)
+    // console.log(`rundate: ${rundate}`)
+    const params: PlotParams = loadPlot(njspPlotSpec)
+    // const projectedTotals = loadSync<ProjectedTotals>(`public/plots/projected_totals.json`)
+    const crashesPqtPath = path.join(dirname(process.cwd()), "data", "crashes.pqt")
+    const crashesPqtBuf = fs.readFileSync(crashesPqtPath)
+    // const crashesPqtArr8 = new Uint8Array(crashesPqtBuf)
+    const crashesPqtArr = Array.from(crashesPqtBuf)
+    const crashesBase64 = crashesPqtBuf.toString('base64') //btoa(crashesPqtArr)//String.fromCharCode.apply(null, crashesPqtArr))
+    console.log("crashesBase64:", crashesBase64.substring(0, 100))
+    return {
+        props: {
+            params,
+            crashesBase64,
+            // crashesPqtArr,
+            /*projectedTotals, rundate,*/
+        },
+    }
+}
+
+const title = "New Jersey Car Crash Deaths"
+
+export default function Page({ params, crashesBase64, /*crashesPqtArr,*/ /*projectedTotals, rundate,*/ }: Props) {
+    const spec = njspPlotSpec
+    let { src, name } = spec
+    src = src ?? `plots/${name}.png`
+    const [ db, setDb ] = useState<AsyncDuckDB | null>(null)
+    const csvUrl = "https://raw.githubusercontent.com/neighbor-ryan/nj-crashes/main/data/njsp/year-type-county.csv"
+    const [ data, setData ] = useState<any[] | null>(null)
+    useEffect(
+        () => {
+            async function init() {
+                const db = await initDuckDb()
+                // const arr = atob(crashesBase64)
+                let crashesPqtArr = new Uint8Array(Buffer.from(crashesBase64, 'base64'))
+                // let crashesPqtArr = new Uint8Array(
+                //     atob(crashesBase64)
+                //         .split("")
+                //         .map(char => char.charCodeAt(0))
+                // )
+                console.log("got db:", db)
+                await db.registerFileBuffer(
+                    'crashes.parquet',
+                    crashesPqtArr,
+                    // Uint8Array.from(crashesPqtArr)
+                )
+                console.log("registered file")
+                const query = `SELECT count(*) FROM parquet_scan('crashes.parquet')`
+                const countRes = await runQuery(db, query)
+                console.log("countRes:", countRes)
+            }
+            init()
+            // initDuckDb(/*{ path:  }*/).then(
+            //     db => {
+            //         console.log("got db:", db)
+            //         setDb(db)
+            //
+            //         const query = `CREATE TABLE ytc AS SELECT * from read_csv_auto('${csvUrl}')`
+            //         return runQuery(db, query).then(res => {
+            //             console.log("made table:", res)
+            //             return runQuery(db, `SELECT * FROM ytc`)
+            //         }).then(data => {
+            //             console.log("got data:", data)
+            //             setData(data)
+            //         })
+            //     }
+            // )
+        },
+        []
+    )
+    return (
+        <div className={css.container}>
+            <Head
+                title={title}
+                description={"Analysis & Visualization of traffic crash data published by NJ State Police and NJ DOT"}
+                url={url}
+                thumbnail={`${url}/plots/fatalities_per_year_by_type.png`}
+            />
+
+            <main className={css.main}>
+                <h1 className={css.title}>{title}</h1>
+                <p>
+                    Data comes from <A
+                        title={"NJ State Police fatal crash data"}
+                        href={"https://nj.gov/njsp/info/fatalacc/"}>
+                        NJ State Police
+                    </A>, and is updated daily (though crashes sometimes take weeks or months to show up).
+                </p>
+                <div className={css["plot-container"]}>
+                    <PlotWrapper
+                        params={params}
+                        src={src}
+                        alt={title}
+                        // margin={{b: 30,}}
+                    />
+                    <hr/>
+                </div>
+                <p>Code and data are <A href={GitHub.href}>on GitHub</A>; feedback / issues <A href={`${GitHub.href}/issues/new`}>here</A>).</p>
+                <Socials
+                    socials={[
+                        GitHub,
+                        // { name: "NJSP", title: "NJ State Police fatal crash data", href: "https://nj.gov/njsp/info/fatalacc/", src: `/njsp.png`, },
+                        // { name: "NJDOT", title: "NJ DOT raw crash data", href: "https://www.state.nj.us/transportation/refdata/accident/rawdata01-current.shtm", src: `/njdot-s.png`, },
+                        {
+                            name: "Hudson County Complete Streets",
+                            title: "Hudson County Complete Streets",
+                            href: "https://hudcostreets.org",
+                            src: `/logos/hccs.png`,
+                        },
+                    ]}
+                />
+            </main>
+        </div>
+    )
+}
