4f6253d6b4dc0535c540b445debcb2f35ac8342d
Ryan Williams <ryan@runsascoded.com>
2024-01-22 15:47:13 -0500

    document parse-ghp-base-sha.py

--- parse-ghp-base-sha.py
+++ parse-ghp-base-sha.py
@@ -11,9 +11,10 @@ RGX = r'.*@(?P<sha>[\da-f]{40}).*'
 
 
 @click.command()
-@click.option('-d', '--default')
+@click.option('-d', '--default', help="Optional fallback (in case the provided <ref> doesn't exist)")
 @click.argument('ref', default='origin/gh-pages')
 def main(default, ref):
+    """Print the SHA of the commit that triggered a given GitHub Pages commit."""
     try:
         msg = check_output(['git', 'log', '-1', '--format=%s', ref, ]).decode().rstrip('\n')
     except CalledProcessError as e:
