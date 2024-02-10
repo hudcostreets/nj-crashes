1001c8d40de2250c4d260f8fbf77012a3c537226
Ryan Williams <ryan@runsascoded.com>
2024-02-09 20:55:52 -0500

    crash_logs: add -i/--in-place, auto-root-detection

--- njsp/cli/crash_logs.py
+++ njsp/cli/crash_logs.py
@@ -4,9 +4,10 @@ import click
 import pandas as pd
 from utz import err
 
+from njdot.rawdata import singleton
 from njsp.cli.base import njsp
 from njsp.crash_log import get_crashes_df, DEFAULT_ROOT_SHA
-
+from njsp.paths import CRASHES_RELPATH
 
 # Enforce column order, otherwise DFs built using 1 or more -a/--append-to chains can have different column orders (e.g.
 # STREET, HIGHWAY, and INJURIES may each appear for the first time, in a given FAUQStats XML file, in different orders).
@@ -24,13 +25,29 @@ COLS = [
 @click.option('-a', '--append-to', help='Append to existing file (typically `crash_logs/<root>.pqt`')
 @click.option("-f", "--write-dupes", is_flag=True, help="Write output even when duplicate rows are detected")
 @click.option('-h', '--head', help='Ref to begin ancestor-traversal from')
+@click.option("-i", "--in-place", is_flag=True, help="Overwrite the input file -a/--append-to")
 @click.option("-o", "--out-path", help="Path to save the output")
-@click.option("-r", "--root", default=DEFAULT_ROOT_SHA, help="Ref to end at")
+@click.option("-p", "--load-parquet", is_flag=True, help=f"Load crashes from {CRASHES_RELPATH} (instead of FAUQStats XML files)")
+@click.option("-r", "--root", help=f"Ref to end at; if -a/--append-to is passed, defaults to the latest SHA in that DataFrame, {DEFAULT_ROOT_SHA} otherwise")
 @click.option("-s", "--since", help="Date to start from")
 @click.option("-v", "--verbose", is_flag=True, help="Print debug info")
-@click.option("-x", "--load-xml", is_flag=True, help="Load crashes from FAUQStats XML files (instead of crashes.pqt)")
-def crash_logs(append_to, write_dupes, head, out_path, root, since, verbose, load_xml):
-    df = get_crashes_df(head=head, root=root, since=since, load_pqt=not load_xml, log=verbose)
+def crash_logs(append_to, write_dupes, head, in_place, out_path, load_parquet, root, since, verbose):
+    if append_to:
+        if not root:
+            prefix = pd.read_parquet(append_to)
+            df_sha = prefix.reset_index(level=0)
+            latest_prefix_sha = df_sha.rundate.idxmax()
+            root = latest_prefix_sha
+            latest_rundate = singleton(df_sha.loc[latest_prefix_sha, 'rundate'].tolist())
+            err(f"Using latest SHA from {append_to} as root: {root} (rundate {latest_rundate})")
+        if in_place:
+            if out_path:
+                raise ValueError("Cannot use -i/--in-place and -o/--out-path together")
+            out_path = append_to
+    elif in_place:
+        raise ValueError("Cannot use -i/--in-place without -a/--append-to")
+
+    df = get_crashes_df(head=head, root=root, since=since, load_pqt=load_parquet, log=verbose)
     cols = [
         col
         for col in COLS
