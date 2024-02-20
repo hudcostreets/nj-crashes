22137023cb8ff9b6f89b80a85403e9da0c708c24
Ryan Williams <ryan@runsascoded.com>
2024-02-20 09:17:32 -0500

    mv data/njsp njsp/data

--- njsp/paths.py
+++ njsp/paths.py
@@ -3,23 +3,25 @@ from os.path import join, relpath
 
 from nj_crashes.paths import DATA_DIR, ROOT_DIR, WWW_NJSP
 
+NJSP_DIR = dirname(__file__)
+
 CRASHES_PQT = join(DATA_DIR, 'crashes.pqt')
 CRASHES_RELPATH = relpath(CRASHES_PQT, ROOT_DIR)
 
-DATA_NJSP = join(DATA_DIR, 'njsp')
-PROJECTED_CSV = join(DATA_NJSP, 'projected.csv')
+NJSP_DATA = join(NJSP_DIR, 'data')
+PROJECTED_CSV = join(NJSP_DATA, 'projected.csv')
 
 # Tabula template JSONs and NJSP summary PDFs live in this dir
-ANNUAL_REPORTS = join(DATA_NJSP, 'annual-reports')
-ANNUAL_SUMMARIES = join(DATA_NJSP, 'annual-summaries')
+ANNUAL_REPORTS = join(NJSP_DATA, 'annual-reports')
+ANNUAL_SUMMARIES = join(NJSP_DATA, 'annual-summaries')
 ANNUAL_YTC = join(ANNUAL_REPORTS, 'year-type-county')
 MISSING_YTC = join(ANNUAL_YTC, 'missing_ytc.csv')
 
 ANNUAL_SUMMARIES_YT_CSV = path.join(ANNUAL_SUMMARIES, 'year-type.csv')
 ANNUAL_SUMMARIES_YTC_CSV = path.join(ANNUAL_SUMMARIES, 'year-type-county.csv')
 
-YTC_CSV = path.join(DATA_NJSP, 'year-type-county.csv')
-YTC_PQT = path.join(DATA_NJSP, 'year-type-county.pqt')
+YTC_CSV = path.join(NJSP_DATA, 'year-type-county.csv')
+YTC_PQT = path.join(NJSP_DATA, 'year-type-county.pqt')
 YTC_DB = path.join(WWW_NJSP, 'year-type-county.db')
 YTC_DB_URI = f'sqlite:///{YTC_DB}'
 
