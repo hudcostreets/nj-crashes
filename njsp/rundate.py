3c076d24cd62f21633f42c9baa7eb1d9321c067e
Ryan Williams <ryan@runsascoded.com>
2024-02-23 21:06:37 -0500

    only localize rundate if necessary

--- njsp/rundate.py
+++ njsp/rundate.py
@@ -18,7 +18,8 @@ class Rundate:
     @cached_property
     def cur(self):
         with open(RUNDATE_PATH, 'r') as f:
-            return pd.to_datetime(json.load(f)['rundate']).tz_localize(TZ)
+            dt = pd.to_datetime(json.load(f)['rundate'])
+            return dt if dt.tz else dt.tz_localize(TZ)
 
     @property
     def cur_month_str(self):
