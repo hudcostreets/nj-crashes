8b8b752e0d4b24baf08d9ede297c939fc75e65d9
Ryan Williams <ryan@runsascoded.com>
2024-02-24 08:46:47 -0500

    refresh_data use fauqstats_relpath

--- njsp/cli/refresh_data.py
+++ njsp/cli/refresh_data.py
@@ -1,15 +1,18 @@
+from os.path import basename
+
 from datetime import datetime
 
 import click
 from utz import process
 
 from .base import command
+from ..paths import fauqstats_relpath
 
 
 def update_years(*years):
     for year in years:
-        name = f"FAUQStats{year}.xml"
-        out_path = f'data/{name}'
+        out_path = fauqstats_relpath(year)
+        name = basename(out_path)
         process.run('wget', '-O', out_path, f'https://nj.gov/njsp/info/fatalacc/{name}')
         process.run('git', 'add', out_path)
 
