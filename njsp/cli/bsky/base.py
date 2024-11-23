29e337c59b8e16c5e358c2ff6cb12441bb874f9b
Ryan Williams <ryan@runsascoded.com>
2024-11-23 18:54:42 -0500

    add cli docstrs

--- njsp/cli/bsky/base.py
+++ njsp/cli/bsky/base.py
@@ -3,4 +3,5 @@ from ..base import njsp
 
 @njsp.group('bsky')
 def bsky():
+    """Manage @crashes.hudcostreets.org on Bluesky."""
     pass
--- njsp/cli/slack/base.py
+++ njsp/cli/slack/base.py
@@ -3,4 +3,5 @@ from ..base import njsp
 
 @njsp.group('slack')
 def slack():
+    """Post crashes to the #crash-bot channel in HCCS Slack."""
     pass
