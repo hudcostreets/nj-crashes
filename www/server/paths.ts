f47b6d5cabe6ba2d5bb6aa03b857caf420236d4f
Ryan Williams <ryan@runsascoded.com>
2024-11-28 09:13:41 -0500

    urls cleanup

--- www/server/paths.ts
+++ www/server/paths.ts
@@ -7,7 +7,6 @@ export const ROOT = dirname(WWW)
 export const NJSP = path.join(PUBLIC, "njsp")
 export const PLOTS = path.join(PUBLIC, "plots")
 export const RUNDATE = path.join(NJSP, "rundate.json")
-export const RUNDATE_RELPATH = path.relative(WWW, RUNDATE)
 
 export const NJDOT = path.join(PUBLIC, "njdot")
 export const CC2MC2MN = path.join(NJDOT, "cc2mc2mn.json")
