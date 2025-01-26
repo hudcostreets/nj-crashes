93271ff6a0b03afb389e153a80b4ab0a37c3ed6d
Ryan Williams <ryan@runsascoded.com>
2025-01-26 16:24:27 -0500

    include shas

--- year-cmp.py
+++ year-cmp.py
@@ -17,14 +17,15 @@ year0 = date0.year
 month = date0.month
 day = date0.day
 
-def load_mtd(year: int, sha: str | None = None) -> Tuple[DF, date]:
+def load_mtd(year: int, sha: str | None = None) -> DF | Tuple[DF, str, date]:
     if not sha:
         sha, sha_date = load_sha_date(year, f'--before={year}-{month:02d}-{day:02d}')
         assert sha_date.month == month
         assert sha_date.day == day
+        return_tpl = True
     else:
         assert sha == sha0
-        sha_date = date0
+        return_tpl = False
     commit = repo.commit(sha)
     blobs = FAUQStats.blobs(commit)
     blob = blobs[year]
@@ -32,26 +33,26 @@ def load_mtd(year: int, sha: str | None = None) -> Tuple[DF, date]:
     crashes = stats.crashes
     dt = crashes.dt.dt
     mtd = crashes[(dt.month <= month) & (dt.day <= day)]
-    return mtd, sha_date
+    return (mtd, sha, sha_date) if return_tpl else mtd
 
 year1 = year0 - 1
 year2 = year0 - 2
-df0, _ = load_mtd(year0, sha0)    # Current YTD crashes
-df1, date1 = load_mtd(year1)      # Last year's YTD crashes, as they appeared on this date last year
-df1_0, _ = load_mtd(year1, sha0)  # Current view of last year's crashes (includes any that were reported after a delay)
-df2, date2 = load_mtd(year2)      # 2ya's YTD crashes, as they appeared on this date that year
-df2_0, _ = load_mtd(year2, sha0)  # Current view of 2ya's crashes (includes any that were reported after a delay)
+df0 = load_mtd(year0, sha0)         # Current YTD crashes
+df1, sha1, date1 = load_mtd(year1)  # Last year's YTD crashes, as they appeared on this date last year
+df1_0 = load_mtd(year1, sha0)       # Current view of last year's crashes (includes any that were reported after a delay)
+df2, sha2, date2 = load_mtd(year2)  # 2ya's YTD crashes, as they appeared on this date that year
+df2_0 = load_mtd(year2, sha0)       # Current view of 2ya's crashes (includes any that were reported after a delay)
 
 #print(f'Comparing {sha0} ({date0}) to {sha1} ({date1})')
 cols = ['FATAL_D', 'FATAL_P', 'FATAL_T', 'FATAL_B']
-def print_summary(df: DF, as_of: date, year: int):
+def print_summary(df: DF, as_of: date, sha: str, year: int):
     sums = df[cols].sum().astype(int)
     type_str = ', '.join([f'{t}: {sums[f"FATAL_{t}"]:>2d}' for t in 'DPTB'])
-    print(f"As of {as_of}, {year} (until {month}/{day}) had {sums.sum()} deaths: {type_str}")
+    print(f"As of {as_of} (commit {sha}), {year} (until {month}/{day}) had {sums.sum()} deaths: {type_str}")
     return sums
 
-print_summary(df0, date0, year0)
-print_summary(df1, date1, year1)
-print_summary(df1_0, date0, year1)
-print_summary(df2, date2, year2)
-print_summary(df2_0, date0, year2)
+print_summary(df0, date0, sha0, year0)
+print_summary(df1, date1, sha1, year1)
+print_summary(df1_0, date0, sha0, year1)
+print_summary(df2, date2, sha2, year2)
+print_summary(df2_0, date0, sha0, year2)
