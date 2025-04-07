58dcd35e09679a1acbf1be96c815c898ad1da903
Ryan Williams <ryan@runsascoded.com>
2025-04-07 00:41:42 -0400

    update GHA slack sync

--- njsp/cli/slack/sync.py
+++ njsp/cli/slack/sync.py
@@ -2,8 +2,10 @@ from typing import Tuple
 
 import pandas as pd
 from click import option, argument
+from utz import solo
 from utz.ymd import dates, YMD
 
+from nj_crashes.utils.git import git_fmt
 from .base import slack, channel_client_opts
 from .channel_client import ChannelClient
 from ...crash import Log
@@ -16,6 +18,7 @@ from ...paths import S3_CRASH_LOG_PQT
 @dates(default_start=YMD(2008), help='Date range to filter crashes to, e.g. `202307-`, `20230710-202308')
 @option('-f', '--overwrite-existing', count=True, help='1x: update existing messages; 2x: delete existing messages and post new ones (default: 0, do nothing)')
 @option('-l', '--crash-log-url', default=S3_CRASH_LOG_PQT, help=f'File containing crash-update history (default: {S3_CRASH_LOG_PQT})')
+@option('-r', '--ref', help='Sync crashes updates at this Git SHA in the crash-log')
 @argument('accids', type=int, nargs=-1)
 def sync(
     client: ChannelClient,
@@ -23,6 +26,7 @@ def sync(
     end: YMD | None,
     overwrite_existing: int,
     crash_log_url: str,
+    ref: str | None,
     accids: Tuple[int, ...],
 ):
     """Post crashes to the #crash-bot channel in HCCS Slack.
@@ -31,7 +35,15 @@ def sync(
     updates `data/FAUQStats*.xml` files).
     """
     crashes_log = pd.read_parquet(crash_log_url)
-    if accids:
+    if ref:
+        if accids:
+            raise ValueError("Cannot specify both --ref and accids")
+        reset = crashes_log.reset_index()
+        l, n = solo(reset.sha.apply(len).value_counts().to_dict())
+        ref = git_fmt(ref, fmt='%H')[:l]
+        accids = reset.loc[reset.sha == ref, 'accid'].unique().tolist()
+        crashes_log = crashes_log.loc[list(accids)]
+    elif accids:
         crashes_log = crashes_log.loc[list(accids)]
 
     first_dates = crashes_log.groupby(level=0)['dt'].min().dt.date
