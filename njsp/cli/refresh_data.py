62705a4f026cc4d996051631705f927f4e6c978e
Ryan Williams <ryanw@hudcostreets.org>
2025-06-05 20:06:02 -0400

    `refresh`: use `requests` instead of `wget`

--- njsp/cli/refresh_data.py
+++ njsp/cli/refresh_data.py
@@ -3,6 +3,7 @@ from os.path import basename
 from datetime import datetime
 
 from click import argument
+import requests
 from utz import process
 
 from .base import command
@@ -13,7 +14,24 @@ def update_years(*years):
     for year in years:
         out_path = fauqstats_relpath(year)
         name = basename(out_path)
-        process.run('wget', '-O', out_path, f'https://nj.gov/njsp/info/fatalacc/{name}')
+        res = requests.get(
+            f'https://nj.gov/njsp/info/fatalacc/{name}',
+            allow_redirects=True,
+            timeout=10,
+            headers={
+                'Accept': 'text/xml',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/',
+            },
+        )
+        if res.status_code != 200:
+            raise ValueError(f"Failed to download {name}: {res.status_code} {res.reason}")
+        if res.headers.get('Content-Type') != 'text/xml':
+            raise ValueError(f"Unexpected content type for {name}: {res.headers.get('Content-Type')}")
+        with open(out_path, 'wb') as f:
+            f.write(res.content)
+
         process.run('git', 'add', out_path)
 
 
