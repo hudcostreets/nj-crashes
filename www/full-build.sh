78c0a05f02693c3a09c5b5e8959b04c673e1c6de
Ryan Williams <ryanw@hudcostreets.org>
2025-06-28 10:50:21 -0400

    `pip install dvc dvc-s3`

--- www/full-build.sh
+++ www/full-build.sh
@@ -3,6 +3,7 @@
 set -ex
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
+pip install dvc dvc-s3
 dvc pull public/njdot/data/{cmymc,crashes,drivers,occupants,pedestrians,vehicles}.db
 aws s3 sync --exclude '*' --include 'crashes.db' s3://nj-crashes/njsp/data/ public/njsp/data/
 npm run build
